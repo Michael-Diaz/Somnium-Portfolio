5fc9723dc354a629323f462b8bff7c8e55cab028
Steve Jacques <steve.jacques@outlook.com>
2022-01-29 17:52:09 -0500

    just pushing for safety

--- SomniumGame/Assets/Scripts/CharacterMovement.cs
+++ SomniumGame/Assets/Scripts/CharacterMovement.cs
@@ -2,26 +2,88 @@ using UnityEngine;
 
 public class CharacterMovement : MonoBehaviour
 {
-    public float MovementSpeed = 1;
-    public float JumpForce = 1;
+    public float moveSpeed;
+    public float jumpForce;
+    private float moveInput;
 
-    private Rigidbody2D _rigidbody;
+    private Rigidbody2D rb;
+    
+    private bool rightOriented = true;
+
+    private bool isGrounded;
+    private bool isStealthed = false;
+    public Transform groundCheck;
+    public float checkRadius;
+    public LayerMask whatIsGround;
+    private int numJumps;
+    public int extraJumps;
+
 
     // Start is called before the first frame update
-    private void Start()
+    void Start()
     {
-        _rigidbody = GetComponent<Rigidbody2D>();    
+        numJumps = extraJumps;
+        rb = GetComponent<Rigidbody2D>();    
     }
 
-    // Update is called once per frame
-    private void Update()
+    void FixedUpdate()
     {
-        var movement = Input.GetAxis("Horizontal");
-        transform.position += new Vector3(movement, 0, 0) * Time.deltaTime * MovementSpeed;
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, checkRadius, whatIsGround);
 
-        if (Input.GetButtonDown("Jump") && Mathf.Abs(_rigidbody.velocity.y) < 0.001f)
+        moveInput = Input.GetAxis("Horizontal");
+        if (isStealthed)
         {
-            _rigidbody.AddForce(new Vector2(0, JumpForce), ForceMode2D.Impulse);
+            rb.velocity = new Vector2(moveInput * moveSpeed * 0.5F, rb.velocity.y);
+        }
+        else
+        {
+            rb.velocity = new Vector2(moveInput * moveSpeed, rb.velocity.y);
+        }
+
+        if (!rightOriented && moveInput > 0)
+        {
+            Flip();
+        }
+        else if (rightOriented && moveInput < 0)
+        {
+            Flip();
         }
     }
+
+    void Update() 
+    {
+        if (Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            isStealthed = !isStealthed;
+        }
+
+        if(isGrounded)
+        {
+            numJumps = extraJumps;
+        }
+
+        if(Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            if (numJumps > 0)
+            {
+                rb.velocity = Vector2.up * jumpForce;
+                numJumps--;
+            }
+            else if (numJumps == 0 && isGrounded)
+            {
+                rb.velocity = Vector2.up * jumpForce;
+            }
+        }
+        
+    }
+
+    void Flip()
+    {
+        rightOriented = !rightOriented;
+        Vector3 Scales = transform.localScale;
+
+        Scales.x *= -1;
+        transform.localScale = Scales;
+    }
+
 }
