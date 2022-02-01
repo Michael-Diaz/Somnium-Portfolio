fd62aafe371bf64d4d0b569f83da52f8a7c870f8
Michael Diaz <michaeldiaz.compsci@gmail.com>
2022-02-01 02:34:54 -0500

    Scene Cleanup
    -Fixed transforms, normals, and coordinates for stairs asset
    -Fixed clipping in the LevelGen scene
    -Altered web cam script to adjust for graphics resolution

--- SomniumGame/Assets/Scripts/DisplayWebCam.cs
+++ SomniumGame/Assets/Scripts/DisplayWebCam.cs
@@ -5,13 +5,18 @@ using UnityEngine.UI;
 
 public class DisplayWebCam : MonoBehaviour
 {
-    public RawImage rawImage;
+    public RawImage footageImage;
+    public AspectRatioFitter footageDimensions;
+
     // Start is called before the first frame update
     void Start()
     {
         WebCamTexture webCam = new WebCamTexture();
-        rawImage.texture = webCam;
-        rawImage.material.mainTexture = webCam;
+        footageImage.texture = webCam;
+        footageImage.material.mainTexture = webCam;
         webCam.Play();
+
+        float videoRatio = (float)webCam.width / (float)webCam.height;
+        footageDimensions.aspectRatio = videoRatio;
     }
 }
