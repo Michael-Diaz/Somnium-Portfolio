884249fe5113c6e7aa43d48e3bb5c25d6b6ef8fe
Grant Allan <45442774+Grant-Allan@users.noreply.github.com>
2022-03-05 13:12:25 -0500

    Update EmotionDetection.cs

--- SomniumGame/Assets/Scripts/Facial_Recognition/EmotionDetection.cs
+++ SomniumGame/Assets/Scripts/Facial_Recognition/EmotionDetection.cs
@@ -1,30 +1,40 @@
-/* A script that takes input from the webcam (or any attached camera)
- * and feeds it into the emotion recognition model.
- *
- * Note: Need to add the option to select your desired camera device.
- */
+/*
+A script that takes input from the webcam (or any attached camera)
+and feeds it into the emotion recognition model.
+
+Note: Need to add the option to select your desired camera device.
+*/
 
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 using System;
+using System.IO;
+using System.Diagnostics;
 using System.Runtime.InteropServices;
 using UnityEngine.UI;
 using Unity.Barracuda;
+
+using OpenCVForUnity.CoreModule;
+using OpenCVForUnity.ImgprocModule;
+using OpenCVForUnity.UnityUtils;
+
 
 public class EmotionDetection : MonoBehaviour
 {
     public RawImage rawImage;
     public NNModel modelAsset;
-    public int prediction; // output
-    public float[] preds;
+    public int prediction; // output index
+    public float[] preds; // output array
 
-    private WebCamTexture _camTexture;
+    private CascadeClassifier cascade;
+    private MatOfRect faces;
+    private WebCamTextureToMatHelper webCamTextureToMatHelper;
+
+    private WebCamTexture _webcamTexture;
     private Model _runtimeModel;
     private IWorker _engine;
-    
-    public AspectRatioFitter footageDimensions;
 
     // Start is called before the first frame update
     void Start()
@@ -33,20 +43,28 @@ public class EmotionDetection : MonoBehaviour
         // For debugging purposes, prints available devices to the console
         WebCamDevice[] cam_devices = WebCamTexture.devices;
         for (int i = 0; i < cam_devices.Length; i++)
-            Debug.Log($"Webcam available: {cam_devices[i].name}");
+            UnityEngine.Debug.Log($"Webcam available: {cam_devices[i].name}");
 
         // Assuming the first available WebCam is desired
-        _camTexture = new WebCamTexture(cam_devices[0].name);
-        rawImage.texture = _camTexture;
-        rawImage.material.mainTexture = _camTexture;
-        if (_camTexture != null)
-            Debug.Log($"Streaming [{cam_devices[0].name}]");
-            _camTexture.Play();
+        _webcamTexture = new WebCamTexture(cam_devices[0].name);
+        rawImage.texture = _webcamTexture;
+        rawImage.material.mainTexture = _webcamTexture;
+
+        if (_webcamTexture != null) {
+            UnityEngine.Debug.Log($"Streaming [{cam_devices[0].name}]");
+            _webcamTexture.Play();
+        }
+
+        // Haar cascade set-up
+        string path = Application.dataPath + @"/Resources/Haar_Cascades/haarcascade_frontalface_default.xml";
+        cascade = new CascadeClassifier();
+        cascade.load(path);
+
+        // Avoids the front camera low light issue that occurs in only some Android devices (e.g. Google Pixel, Pixel2).
+        webCamTextureToMatHelper.avoidAndroidFrontCameraLowLightIssue = true;
+        webCamTextureToMatHelper.Initialize();
         
-        float videoRatio = (float)_camTexture.width / (float)_camTexture.height;
-        footageDimensions.aspectRatio = videoRatio;
-        
-        // Set up the model
+        // Emotion detection model set-up
         _runtimeModel = ModelLoader.Load(modelAsset);
         _engine = WorkerFactory.CreateWorker(_runtimeModel, WorkerFactory.Device.Auto);
     }
@@ -54,54 +72,150 @@ public class EmotionDetection : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Debug.Log(" EmotionModel Update Called ====================================");
+        UnityEngine.Debug.Log(" EmotionModel Update Called ====================================");
 
-        /*
-        // Convert camera input directly into a texture
-        Texture2D tx2d  = new Texture2D(_camTexture.width, _camTexture.height);
-        tx2d.SetPixels(_camTexture.GetPixels());
-        tx2d.Apply();
-        */
+        // Get the Mat from the current webcam frame
+        Mat rgbaMat = new Mat(_webcamTexture.height, _webcamTexture.width, CvType.CV_8UC5); // empty 0-255, rgba mat
+        Mat grayMat = new Mat(_webcamTexture.height, _webcamTexture.width, CvType.CV_8UC1); // empty 0-255, grayscale mat
+        Utils.webCamTextureToMat(_webcamTexture, grayMat);
+
+        // Fill faces. Also, resize grayMat for superior performance.
+        cascade.detectMultiScale(grayMat, faces, 1.1, 2, 2, new Size(grayMat.cols() * 0.2, grayMat.rows() * 0.2), new Size());
+
+        OpenCVForUnity.CoreModule.Rect[] rects = faces.toArray();
+        for (int i = 0; i < rects.Length; i++)
+            Imgproc.rectangle(rgbaMat, new Point(rects[i].x, rects[i].y), new Point(rects[i].x + rects[i].width, rects[i].y + rects[i].height), new Scalar(255, 0, 0, 255), 2);
         
-        // Crop image to fit input
-        Color[] pixels = _camTexture.GetPixels();
-        int size = 64;
-        Texture2D tx2d  = new Texture2D(size, size);
-        tx2d.SetPixels(pixels);
+        Texture2D texture = new Texture2D(_webcamTexture.height, _webcamTexture.width);
+        Utils.matToTexture2D(rgbaMat, texture);
+
+
+
+
+
+
+
+
+
+        // Save the current camera frame as a .png
+        string path = Application.dataPath + @"/Resources/Model_Images/Cur_Frame/frame.jpg";
+        SaveImage(path);
+        UnityEngine.Debug.Log("Saved Frame");
+
+        // Find faces in the frame
+        RunFile();
+
+        // Load faces
+        Texture2D[] faces = Resources.LoadAll<Texture2D>("Model_Images/Cur_Faces/");
+        UnityEngine.Debug.Log($"Loaded {faces.Length} Faces");
+
+        if (faces.Length == 0)
+            prediction = -1;
+        else
+        {
+            // Loop for detection emotions on detected faces
+            for (int i = 0; i < faces.Length; i++) {
+                // Create input tensor from tx2d
+                int channelCount = 1; // you can treat input pixels as 1 (grayscale), 3 (color) or 4 (color with alpha) channels
+                Tensor inputX = new Tensor(faces[i], channelCount);
+
+                // Input the tensor and get the output
+                Tensor outPreds = _engine.Execute(inputX).PeekOutput();
+                inputX.Dispose();
+
+                // Set public vars
+                prediction = outPreds.ArgMax()[0];
+                preds = outPreds.AsFloats();
+                outPreds.Dispose();
+
+                // neutral=0, happiness=1, surprise=2, sadness=3, anger=4, disgust=5, fear=6, contempt=7
+                if (prediction==0)
+                    UnityEngine.Debug.Log($"Predicted Index: {prediction} (neutral)");
+                else if (prediction==1)
+                    UnityEngine.Debug.Log($"Predicted Index: {prediction} (happiness)");
+                else if (prediction==2)
+                    UnityEngine.Debug.Log($"Predicted Index: {prediction} (surprise)");
+                else if (prediction==3)
+                    UnityEngine.Debug.Log($"Predicted Index: {prediction} (sadness)");
+                else if (prediction==4)
+                    UnityEngine.Debug.Log($"Predicted Index: {prediction} (anger)");
+                else if (prediction==5)
+                    UnityEngine.Debug.Log($"Predicted Index: {prediction} (disgust)");
+                else if (prediction==6)
+                    UnityEngine.Debug.Log($"Predicted Index: {prediction} (fear)");
+                else if (prediction==7)
+                    UnityEngine.Debug.Log($"Predicted Index: {prediction} (contempt)");
+                UnityEngine.Debug.Log($"Prediction Confidence: {preds[prediction]}");
+            }
+        }
+
+        // Clear the face(s) in the Cur_Faces folder
+        ClearCur_Faces();
+        foreach (Texture2D face in faces)
+            Texture2D.DestroyImmediate(face, true);
+    }
+
+    private static void RunFile()
+    {
+        Process Proc = new Process();
+        string path = Application.dataPath + @"/Resources/Emotion_Detector/dist/";
+        Proc.StartInfo.FileName = path + "imgproc.exe";
+        Proc.StartInfo.WorkingDirectory = path;
+        Proc.Start();
+    }
+
+    public Texture2D GetTexture2DFromWebcamTexture(WebCamTexture webCamTexture)
+    {
+        // Create new texture2d
+        Texture2D tx2d = new Texture2D(_webcamTexture.width, _webcamTexture.height);
+        // Gets all color data from web cam texture and then Sets that color data in texture2d
+        tx2d.SetPixels(webCamTexture.GetPixels());
+        // Applying new changes to texture2d
         tx2d.Apply();
+        return tx2d;
+    }
 
-        // Create input tensor from tx2d
-        int channelCount = 1; // you can treat input pixels as 1 (grayscale), 3 (color) or 4 (color with alpha) channels
-        Tensor inputX = new Tensor(tx2d, channelCount);
-        Destroy(tx2d);
 
-        // Input the tensor and get the output
-        Tensor outPreds = _engine.Execute(inputX).PeekOutput();
-        inputX.Dispose();
+    public Tensor MakeInputTensor(Texture2D frame)
+    {
+        // (0, 255) pixel values
+        Color32[] pixels = frame.GetPixels32();
 
-        // Set public vars
-        prediction = outPreds.ArgMax()[0];
-        preds = outPreds.AsFloats();
-        outPreds.Dispose();
+        // (0, 1) pixel values
+        //Color[] pix = frame.GetPixels();
 
-        // neutral=0, happiness=1, surprise=2, sadness=3, anger=4, disgust=5, fear=6, contempt=7
-        if (prediction==0)
-            Debug.Log($"Predicted Index: {prediction} (neutral)");
-        else if (prediction==1)
-            Debug.Log($"Predicted Index: {prediction} (happiness)");
-        else if (prediction==2)
-            Debug.Log($"Predicted Index: {prediction} (surprise)");
-        else if (prediction==3)
-            Debug.Log($"Predicted Index: {prediction} (sadness)");
-        else if (prediction==4)
-            Debug.Log($"Predicted Index: {prediction} (anger)");
-        else if (prediction==5)
-            Debug.Log($"Predicted Index: {prediction} (disgust)");
-        else if (prediction==6)
-            Debug.Log($"Predicted Index: {prediction} (fear)");
-        else if (prediction==7)
-            Debug.Log($"Predicted Index: {prediction} (contempt)");
-        Debug.Log($"Prediction Confidence: {preds[prediction]}");
+        // Create empty float array of the needed length
+        int inputHeight = 640;
+        int inputWidth = 640;
+        int numChannels = 3;
+        float[] floats = new float[inputHeight * inputWidth * numChannels];
+
+        // Fill the empty float array with properly scaled pixel values
+        for (int i = 0; i < pixels.Length; ++i)
+        {
+            var color = pixels[i];
+
+            // Value range: (0, 255)
+            floats[i * numChannels + 0] = color.r;
+            floats[i * numChannels + 1] = color.g;
+            floats[i * numChannels + 2] = color.b;
+
+            // Value range: (0, 1)
+            //floats[i * numChannels + 0] = (color.r / 255f);
+            //floats[i * numChannels + 1] = (color.g / 255f);
+            //floats[i * numChannels + 2] = (color.b / 255f);
+
+            // Value range: (-1, 1)
+            //floats[i * numChannels + 0] = (color.r - 127) / 127.5f;
+            //floats[i * numChannels + 1] = (color.g - 127) / 127.5f;
+            //floats[i * numChannels + 2] = (color.b - 127) / 127.5f;
+
+        }
+
+        // Create the input tensor from pixel values
+        // Batch, height, width, channels, pixel data
+        Tensor inputTensor = new Tensor(1, inputHeight, inputWidth, numChannels, floats);
+        return inputTensor;
     }
 
     private void OnDestroy()
