92e01545b9e97d8ed4f678f2eb1c735174e08a3c
Grant Allan <45442774+Grant-Allan@users.noreply.github.com>
2022-03-16 20:43:08 -0400

    Current state of things

--- SomniumGame/Assets/Scripts/Scene_Loader/StartGame.cs
+++ SomniumGame/Assets/Scripts/Scene_Loader/StartGame.cs
@@ -1,14 +1,69 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class StartGame : MonoBehaviour
 {
-    // Start a new game
-    public void StartNewGame()
+    public GameObject mainMenuCanvas;
+    public GameObject loadingScreenCanvas;
+    public Slider slider;
+
+
+    public void NewGame(string levelToLoad) 
     {
-        // Since this is a new game, this is all that's needed
-        SceneManager.LoadSceneAsync("LevelGen"); // load the Scene in the background
+        // Get the old (current) scene
+        Scene old_scene = SceneManager.GetActiveScene();
+        Debug.Log($"Current Active Scene: {old_scene.name}");
+        
+        // Load new scene
+        StartCoroutine(LoadAsynchronously(levelToLoad, old_scene));
+    }
+
+
+    IEnumerator LoadAsynchronously(string levelToLoad, Scene old_scene)
+    {
+        // Set the loading of the level as an async operation
+        AsyncOperation operation = SceneManager.LoadSceneAsync(levelToLoad, LoadSceneMode.Single);
+
+        while(!operation.isDone)
+        {
+            // Get the 0-1 progress value
+            float progress = Mathf.Clamp01(operation.progress / 0.9f);
+            
+            // Apply the progress value to the slider, which also goes from 0-1
+            slider.value = progress;
+
+            // Going to have a thing here to show the percentage completed as text
+
+            // Unload the main menu
+            yield return UnloadAsynchronously(levelToLoad, old_scene);
+        }
+    }
+
+
+    IEnumerator UnloadAsynchronously(string levelToLoad, Scene old_scene)
+    {
+        // See what scenes are loaded
+        int countLoaded = SceneManager.sceneCount;
+        for (int i = 0; i < countLoaded; i++)
+        {
+            Scene loadedScene = SceneManager.GetSceneAt(i);
+            Debug.Log($"Currently Loaded Scenes: {loadedScene.name}");
+        }
+
+        // Set the newly loaded scene as active
+        SceneManager.SetActiveScene(SceneManager.GetSceneByName(levelToLoad));
+        Scene cur_scene = SceneManager.GetActiveScene();
+        Debug.Log($"Current Active Scene: {cur_scene.name}");
+
+        // Set the loading of the level as an async operation
+        AsyncOperation operation = SceneManager.UnloadSceneAsync(old_scene);
+
+        while(!operation.isDone)
+        {
+            // End the Coroutine
+            yield return null;
+        }
     }
 }
