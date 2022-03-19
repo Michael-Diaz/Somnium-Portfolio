66ba88a3d8f4b1a3f07a806cd2a40728ad231509
Grant Allan <45442774+Grant-Allan@users.noreply.github.com>
2022-03-19 17:27:36 -0400

    Back to Main Menu button in settings

---
+++ SomniumGame/Assets/Scripts/Scene_Loader/ReturnToMainMenu.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class ReturnToMainMenu : MonoBehaviour
+{
+    public string sceneToLoad;
+    public string sceneToUnload;
+    public bool loadAsync=false;
+    public bool additive=false;
+    public bool setActive=false;
+
+
+    public void LoadScene()
+	{
+        // Prepare to load the currently active scene if no scene was specified
+		if (sceneToLoad == null)
+		{
+			sceneToLoad = SceneManager.GetActiveScene().name;
+		}
+
+        // Load the scene
+        SceneManager.LoadScene(sceneToLoad, additive ? LoadSceneMode.Additive : 0);
+
+        // If this scene is also to be set as the active scene...
+		if (setActive)
+		{
+            // Mark it active we have to wait a frame for it to load
+			CallAfterDelay.Create(0, () => {
+				SceneManager.SetActiveScene(SceneManager.GetSceneByName(sceneToLoad));
+			});
+
+            // Unload old scene after we wait a frame
+			CallAfterDelay.Create(0, () => {
+				UnloadScene();
+			});
+		}
+	}
+
+
+	public void UnloadScene()
+	{
+		SceneManager.UnloadSceneAsync(sceneToUnload);
+    }
+}
