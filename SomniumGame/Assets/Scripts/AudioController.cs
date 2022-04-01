c4a79c16a83b5413c1baee7db777ea43f54a5c47
Nicole Freites <nicolejfreites@gmail.com>
2022-04-01 09:12:17 -0400

    Adjusted main menu UI elements

--- SomniumGame/Assets/Scripts/AudioController.cs
+++ SomniumGame/Assets/Scripts/AudioController.cs
@@ -7,7 +7,6 @@ using UnityEngine.Audio;
 public class AudioController : MonoBehaviour
 {
     private static readonly string FirstPlay = "FirstPlay";
-    private static readonly string MasterVol = "MasterVolume";
     private static readonly string MusicVol = "MusicVolume";
     private static readonly string SFXVol = "SFXVolume";
 
@@ -15,12 +14,10 @@ public class AudioController : MonoBehaviour
     private float musicVol, sfxVol, masterVol;
 
     [Header("Audio Sources")]
-    [SerializeField] private AudioSource[] master;
     [SerializeField] private AudioSource[] musicAudios;
     [SerializeField] private AudioSource[] sfxAudios;
 
     [Header("Audio Sliders")]
-    [SerializeField] private Slider masterSlider;
     [SerializeField] private Slider musicSlider;
     [SerializeField] private Slider sfxSlider;
 
@@ -31,21 +28,16 @@ public class AudioController : MonoBehaviour
 
         if (firstPlayInt == 0)
         {
-            masterVol = 0.3f;
             musicVol = 0.3f;
             sfxVol = 0.3f;
-            masterSlider.value = masterVol;
             musicSlider.value = musicVol;
             sfxSlider.value = sfxVol;
-            PlayerPrefs.SetFloat(MasterVol, masterVol);
             PlayerPrefs.SetFloat(MusicVol, musicVol);
             PlayerPrefs.SetFloat(SFXVol, sfxVol);
             PlayerPrefs.SetInt(FirstPlay, -1);
         }
         else
         {
-            masterVol = PlayerPrefs.GetFloat(MasterVol);
-            masterSlider.value = masterVol;
             musicVol = PlayerPrefs.GetFloat(MusicVol);
             musicSlider.value = musicVol;
             sfxVol = PlayerPrefs.GetFloat(SFXVol);
@@ -55,18 +47,12 @@ public class AudioController : MonoBehaviour
 
     public void SaveSoundSettings()
     {
-        PlayerPrefs.SetFloat(MasterVol, masterSlider.value);
         PlayerPrefs.SetFloat(MusicVol, musicSlider.value);
         PlayerPrefs.SetFloat(SFXVol, sfxSlider.value);
     }
 
     public void UpdateVolume()
     {
-        for (int i = 0; i < master.Length; i++)
-        {
-            master[i].volume = masterSlider.value;
-        }
-
         for (int i = 0; i < musicAudios.Length; i++)
         {
             musicAudios[i].volume = musicSlider.value;
