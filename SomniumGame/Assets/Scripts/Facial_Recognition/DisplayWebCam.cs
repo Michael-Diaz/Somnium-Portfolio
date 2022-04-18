249d145f54d7a208a87aa5363958376aa5e9f4b6
Grant Allan <45442774+Grant-Allan@users.noreply.github.com>
2022-04-18 10:27:50 -0400

    Update DisplayWebCam.cs

--- SomniumGame/Assets/Scripts/Facial_Recognition/DisplayWebCam.cs
+++ SomniumGame/Assets/Scripts/Facial_Recognition/DisplayWebCam.cs
@@ -1,7 +1,7 @@
 using UnityEngine;
 using UnityEngine.UI;
 
-public class DisplayWebcam : MonoBehaviour
+public class DisplayWebCam : MonoBehaviour
 {
     /* PUBLICS */
     public GameObject UniversalWebcam;
