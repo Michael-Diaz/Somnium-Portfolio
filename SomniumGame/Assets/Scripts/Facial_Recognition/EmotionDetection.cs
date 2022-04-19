61aba5a50307a7db9ba0bfc48523a943497416b0
Grant Allan <45442774+Grant-Allan@users.noreply.github.com>
2022-04-18 20:00:29 -0400

    Update EmotionDetection.cs

--- SomniumGame/Assets/Scripts/Facial_Recognition/EmotionDetection.cs
+++ SomniumGame/Assets/Scripts/Facial_Recognition/EmotionDetection.cs
@@ -69,7 +69,7 @@ public class EmotionDetection : MonoBehaviour
         _webcamTexture = UniversalWebcam.GetComponent<UniversalWebcam>().webcamTexture;
 
         // Haar cascade set-up
-        string path = Application.dataPath + @"/Models/haarcascade_frontalface_default.xml";
+        string path = Application.dataPath + @"/Resources/haarcascade_frontalface_default.xml";
         _cascade = new CascadeClassifier(path);
 
         try {
