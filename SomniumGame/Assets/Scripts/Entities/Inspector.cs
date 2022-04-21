f7c1b9cc96d34b2e800e834127d75f80bbf6b951
Steve Jacques <steve.jacques@outlook.com>
2022-04-21 03:57:50 -0400

    snitch implemented
    - inspector teleportation and stalker notification put in

--- SomniumGame/Assets/Scripts/Entities/Inspector.cs
+++ SomniumGame/Assets/Scripts/Entities/Inspector.cs
@@ -32,6 +32,12 @@ public class Inspector : MonoBehaviour
     private int destinationFloor;
     private int playerFloor;
     private float playerYPos;
+    private float playerXPos;
+
+    private bool playerSeen = false;
+    private Vector3 playerPos;
+    private Vector3 stalkerPos;
+    public Vector3 playerLastSeen;
 
 
     // Start is called before the first frame update
@@ -45,13 +51,17 @@ public class Inspector : MonoBehaviour
         lowerBound = GameObject.FindGameObjectWithTag("lowerBound").GetComponent<Transform>();
         upperBound = GameObject.FindGameObjectWithTag("upperBound").GetComponent<Transform>();
 
-        InvokeRepeating("path", 3, 3);
+        InvokeRepeating("path", 7, 7);
     }
 
     // Update is called once per frame
     void Update()
     {
-        playerYPos = GameObject.Find("Dreamer").GetComponent<Player>().transform.position.y;
+        stalkerPos = GameObject.Find("Stalker(Clone)").GetComponent<Stalker>().transform.position;
+        playerPos = GameObject.Find("Dreamer").GetComponent<Player>().transform.position;
+        playerYPos = playerPos.y;
+        playerXPos = playerPos.x;
+
         playerFloor = (int) Math.Floor(playerYPos / 3.447346f) + 1;
     }
 
@@ -64,14 +74,38 @@ public class Inspector : MonoBehaviour
     {
         // sets the destination floor of the inspector; it'll teleport withing a floor of the player
         // if the player is on the bottom or top floor it will stay within the bounds [0, mapHeight]
-        destinationFloor = UnityEngine.Random.Range(Math.Max(playerFloor - 1, 0), Math.Min(playerFloor + 1, mapHeight));
-        Debug.Log("player floor" + playerFloor);
-        
-        randomX = ((UnityEngine.Random.Range(1, mapWidth) *  4.0f) - 2.0f);
-        randomY = ((destinationFloor * 3.447346f) + 1.0f);
+        if (!playerSeen)
+        {
+            destinationFloor = UnityEngine.Random.Range(Math.Max(playerFloor - 1, 0), Math.Min(playerFloor + 1, mapHeight));
+            // Debug.Log("player floor" + playerFloor);
+            
+            randomX = ((UnityEngine.Random.Range(1, mapWidth) *  4.0f) - 1.5f);
+            randomY = ((destinationFloor * 3.447346f) + 1.0f);
 
-        transform.position = new Vector3(randomX, randomY, -1.1f);
+            transform.position = new Vector3(randomX, randomY, 0f);
+            currentFloor = (int) Math.Floor(transform.position.y / 3.447346f) + 1;
 
+            if ( (currentFloor == playerFloor) && (Math.Abs(transform.position.x - playerXPos) <= 4))
+            {
+                Debug.Log("seen");
+                playerSeen = true;
+                playerLastSeen = GameObject.Find("Dreamer").GetComponent<Player>().transform.position;
+            }
+        } 
+        else if (playerSeen)
+        {
+            playerSeen = false;
+            transform.position = new Vector3(stalkerPos.x, stalkerPos.y, 0f);
+            // Invoke("snitch", 1);
+            snitch();
+        }
+
+    }
+
+    public void snitch()
+    {
+        GameObject.Find("Stalker(Clone)").GetComponent<Stalker>().updatePos(playerLastSeen.x, playerLastSeen.y);
+        GameObject.Find("Stalker(Clone)").GetComponent<Stalker>()._isInPursuit = true;
     }
 
     public void attack()
